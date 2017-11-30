b56422bf67ac3bbbca1376f848bf8cf96e70559f
Matt Condon <mattgcondon@gmail.com>
2017-11-30 12:54:48 +0200

    fix: update zeppelin-solidity and use their splitpayment

--- token/contracts/DonationSplitter.sol
+++ token/contracts/DonationSplitter.sol
@@ -1,15 +1,5 @@
 pragma solidity 0.4.18;
 
-import './SplitPayment.sol';
+import 'zeppelin-solidity/contracts/payment/SplitPayment.sol';
 
-contract DonationSplitter is SplitPayment {
-    function DonationSplitter (address[] _payees, uint256[] _shares)
-        SplitPayment(_payees, _shares)
-        public
-    {
-
-    }
-
-    // accept ether
-    function () payable {}
-}
+contract DonationSplitter is SplitPayment {}
